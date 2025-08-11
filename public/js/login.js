2377c6118f2c515097fa5bcc3deebe137b2b4b2c
Olalekan Ogundimu <ogundimuolalekan55@gmail.com>
2025-08-11 17:54:32 +0100

    worked on user sign in and authentication

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,25 @@
-const container = document.querySelector('.container');
-const registerBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.login-btn');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add('active');
-})
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.getElementById('next-btn');
+const skipBtn = document.getElementById('skip-btn');
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove('active');
-})
+let currentSlide = 0;
+
+nextBtn.addEventListener('click', () => {
+  slides[currentSlide].classList.remove('active');
+  currentSlide++;
+  
+  if (currentSlide < slides.length) {
+    slides[currentSlide].classList.add('active');
+  } else if (currentSlide === slides.length){
+    window.location.href = "https://us-east-1xw4lugldc.auth.us-east-1.amazoncognito.com/login?client_id=68ea3udqj0n9pd5eufmkrgd9d9&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Folalekanogundimu-4om9.vercel.app%2F"; // Redirect to Cognito UI
+  }
+});
+
+skipBtn.addEventListener('click', () => {
+  window.location.href = "https://us-east-1xw4lugldc.auth.us-east-1.amazoncognito.com/login?client_id=68ea3udqj0n9pd5eufmkrgd9d9&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Folalekanogundimu-4om9.vercel.app%2F";
+});
+
+
+
+
